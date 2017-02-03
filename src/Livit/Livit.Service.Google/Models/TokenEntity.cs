bfd7194f34f2c2246e445dd3e9c14e260a8bfb6d
Nguyen Thanh Cong <mitom3bua@gmail.com>
2017-02-03 17:05:30 +0700

    - Add module to work with Google Calendar

---
+++ src/Livit/Livit.Service.Google/Models/TokenEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Livit.Service.Google.Models
+{
+    using Newtonsoft.Json;
+
+    public class TokenEntity
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+
+        public string ExpiredIn { get; set; }
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+    }
+}
