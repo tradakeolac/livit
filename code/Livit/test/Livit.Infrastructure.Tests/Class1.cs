cdcae8b6cbb0e76c0ff3396bc3018c9e9e5f2d28
Nguyen Thanh Cong <mitom3bua@gmail.com>
2017-02-09 09:28:34 +0700

    - Add the test project

---
+++ code/Livit/test/Livit.Infrastructure.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livit.Infrastructure.Tests
+{
+    [TestClass]
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+    }
+}
